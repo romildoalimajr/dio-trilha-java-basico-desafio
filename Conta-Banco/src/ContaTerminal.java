d48fd53099c8a37513b8e88186e8d7aa8ac77e5b
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-10 22:20:13 -0300

    Criado a classe terminal.

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da conta.: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência.: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o nome do Cliente.: ");
+        String nomeCliente = scanner.next();
+
+        double saldo = 237.48;
+
+        System.out.println("Olá " + nomeCliente + 
+        " obrigado por criar uma conta em nosso banco, sua agência é "
+        + agencia + ", conta " 
+        + numero + " e seu saldo de R$ " 
+        + saldo + " já está disponível para saque");
+
     }
 }
